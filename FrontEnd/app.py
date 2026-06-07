b0d8b83ce29294098a55fe4ac105cc25d096209c
Zeeshan499802 <muhammadzeeshanhameed123@gmail.com>
2026-06-07 14:33:00 +0500

    Add API For FrontEnd

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+# API_URL = "http://127.0.0.1:8000"
+API_URL = "https://ai-meeting-assitant-63ne.onrender.com"
 
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(
